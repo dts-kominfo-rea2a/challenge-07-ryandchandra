b8d2ba19f6fd405e79c972520d180e6b7df7b71f
ryandchandra <ryandchandra1@gmail.com>
2022-06-12 18:43:21 +0800

    feat: finish promiseOutput on promise.js

--- promise.js
+++ promise.js
@@ -1,7 +1,30 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (parameter) => {
+  // ambil data
+  const ixx = await promiseTheaterIXX();
+  const vgc = await promiseTheaterVGC();
+
+  // olah data
+  let counter = 0;
+  
+  // cek emosi dari ixx
+  for (let i = 0; i < ixx.length; i++){
+    if (ixx[i].hasil === parameter){
+      counter++;
+    }
+  }
+
+  // cek emosi dari vgc
+  for (let i = 0; i < vgc.length; i++){
+    if (vgc[i].hasil === parameter){
+      counter++;
+    }
+  }
+
+  return counter;
+};
 
 module.exports = {
   promiseOutput,
